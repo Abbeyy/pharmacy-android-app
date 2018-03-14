264c416aa67d248e70896932c94e214446d7d1d7
Abbey Ross <RossA9@Cardiff.ac.uk>
2018-03-14 13:43:50 +0000

    Created new activity for user story 7: to produce a list of pharmacies offering services in welsh language.

---
+++ app/src/main/java/com/nsa/welshpharmacy/ListPharmaciesActivity.java
@@ -0,0 +1,23 @@
+package com.nsa.welshpharmacy;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by c1714546 on 3/14/2018.
+ *
+ * This class is the Activity behind User Story 7 on
+ * our Taiga Sprint 1 Taskboard. The aim of this
+ * activity is to offer a GUI to the user, on
+ * which a list is offered to the user of
+ * Pharmacies offering services through the
+ * medium of the Welsh language.
+ *
+ * @author Abbey Ross.
+ * @version 1.0 March 14th, 2018.
+ */
+
+public class ListPharmaciesActivity extends AppCompatActivity {
+
+
+
+}
