2c6b31d425f9b2a502192edf36d191522771fb6c
Holly Smith <SmithHL2@cardiff.ac.uk>
2018-03-17 14:23:40 +0000

    Postcode input validation

--- app/src/main/java/com/nsa/welshpharmacy/FilterPreferenceActivity.java
+++ app/src/main/java/com/nsa/welshpharmacy/FilterPreferenceActivity.java
@@ -7,8 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by c1712480 on 14/03/2018.
@@ -38,7 +45,7 @@ public class FilterPreferenceActivity extends AppCompatActivity implements View.
         this.checkHealthCheck = this.findViewById(R.id.check_health_check);
         this.checkSmoking = this.findViewById(R.id.check_smoking);
         this.checkAlcohol = this.findViewById(R.id.check_alcohol);
-        this.textWidget = this.findViewById(R.id.widget_text);
+        this.textWidget = this.findViewById(R.id.text_postcode);
 
         this.submitButton = this.findViewById(R.id.submit_button);
         this.resetButton = this.findViewById(R.id.reset_button);
@@ -47,6 +54,7 @@ public class FilterPreferenceActivity extends AppCompatActivity implements View.
 
         initValues();
 
+        textWidget.setOnClickListener(this);
         submitButton.setOnClickListener(this);
         resetButton.setOnLongClickListener(this);
 
@@ -80,18 +88,24 @@ public class FilterPreferenceActivity extends AppCompatActivity implements View.
     @Override
     public void onClick(View view){
         int id = view.getId();
+        Pattern POSTCODE_REGEX = Pattern.compile(getString(R.string.postcode_regex));
 
-        if (id == R.id.submit_button && this.sharedPreferences != null){
-            SharedPreferences.Editor editor = this.sharedPreferences.edit();
-            editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_AILMENTS, this.checkMinorAilments.isChecked());
-            editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_FLU, this.checkFluVac.isChecked());
-            editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_HEALTH, this.checkHealthCheck.isChecked());
-            editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_SMOKING, this.checkSmoking.isChecked());
-            editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_ALCOHOL, this.checkAlcohol.isChecked());
-            editor.apply();
+        Matcher matcher = POSTCODE_REGEX.matcher(textWidget.getText());
+        if (!matcher.matches()) {
+            Toast.makeText(this, R.string.postcode_invalid, Toast.LENGTH_SHORT).show();
+        }else{
+            if (id == R.id.submit_button && this.sharedPreferences != null){
+                SharedPreferences.Editor editor = this.sharedPreferences.edit();
+                editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_AILMENTS, this.checkMinorAilments.isChecked());
+                editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_FLU, this.checkFluVac.isChecked());
+                editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_HEALTH, this.checkHealthCheck.isChecked());
+                editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_SMOKING, this.checkSmoking.isChecked());
+                editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_ALCOHOL, this.checkAlcohol.isChecked());
+                editor.apply();
 
-            Intent pharmacyListView = new Intent(this, ListPharmaciesActivity.class);
-            startActivity(pharmacyListView);
+                Intent pharmacyListView = new Intent(this, ListPharmaciesActivity.class);
+                startActivity(pharmacyListView);
+            }
         }
     }
     @Override
@@ -113,6 +127,5 @@ public class FilterPreferenceActivity extends AppCompatActivity implements View.
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s){
 
         Toast.makeText(this, R.string.on_shared_pref_saved_text, Toast.LENGTH_SHORT).show();
-
     }
 }
--- app/src/main/java/com/nsa/welshpharmacy/KeyValueHelper.java
+++ app/src/main/java/com/nsa/welshpharmacy/KeyValueHelper.java
@@ -11,6 +11,6 @@ public class KeyValueHelper {
     public static final String KEY_CHECKBOX_SMOKING = "checkbox_smoking";
     public static final String KEY_CHECKBOX_ALCOHOL = "checkbox_alcohol";
     public static final boolean DEFAULT_WIDGET_CHECKBOX = false;
-    //public static final String KEY_WIDGET_TEXT = "text_widget";
-    //public static final String DEFAULT_WIDGET_TEXT = "";
+    public static final String KEY_WIDGET_TEXT = "text_widget";
+    public static final String DEFAULT_WIDGET_TEXT = "";
 }
