b47a6b0f03836376542aea764b9f739f8fdd0d18
Mukhtar Mohamed <Mohamedm4@cardiff.ac.uk>
2018-03-17 07:37:50 +0000

    I have created the default toggle buttons to help declare the availibility of services in welsh

--- app/src/main/java/com/nsa/welshpharmacy/ServicesInWelshActivity.java
+++ app/src/main/java/com/nsa/welshpharmacy/ServicesInWelshActivity.java
@@ -1,8 +1,18 @@
 package com.nsa.welshpharmacy;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
 /**
  * Created by c1502032 on 15/03/2018.
  */
 
-public class ServicesInWelshActivity {
+public class ServicesInWelshActivity extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actvity_services_in_welsh);
+    }
+
 }
+
