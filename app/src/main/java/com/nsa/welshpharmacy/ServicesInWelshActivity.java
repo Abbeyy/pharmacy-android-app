eee4e7a51ff5c9f8c9a25dd869ee0494dbad6ec1
Mukhtar Mohamed <Mohamedm4@cardiff.ac.uk>
2018-03-19 14:40:20 +0000

    toggle buttons are aligned to the left and list view of services created

--- app/src/main/java/com/nsa/welshpharmacy/ServicesInWelshActivity.java
+++ app/src/main/java/com/nsa/welshpharmacy/ServicesInWelshActivity.java
@@ -3,9 +3,15 @@ package com.nsa.welshpharmacy;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.ToggleButton;
+import android.support.v7.widget.ListViewCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by c1502032 on 15/03/2018.
@@ -13,15 +19,47 @@ import android.widget.ToggleButton;
 
 public class ServicesInWelshActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
-    RelativeLayout relativeLayout;
-    ToggleButton toggleButton;
+    LinearLayoutCompat layout;
+    ToggleButton toggleButton1;
+    ToggleButton toggleButton2;
+    ToggleButton toggleButton3;
+    ToggleButton toggleButton4;
+    ToggleButton toggleButton5;
+    ListViewCompat listView;
+    List<String> servicesInWelsh;
+    ArrayAdapter<String> arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actvity_services_in_welsh);
-        toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
-        relativeLayout = (RelativeLayout) findViewById(R.id.layout);
-        toggleButton.setOnCheckedChangeListener(this);
+        toggleButton1 = (ToggleButton) findViewById(R.id.toggleButton);
+        toggleButton2 = (ToggleButton) findViewById(R.id.toggleButton2);
+        toggleButton3 = (ToggleButton) findViewById(R.id.toggleButton3);
+        toggleButton4 = (ToggleButton) findViewById(R.id.toggleButton4);
+        toggleButton5 = (ToggleButton) findViewById(R.id.toggleButton5);
+        layout = (LinearLayoutCompat) findViewById(R.id.layout);
+        this.listView = this.findViewById(R.id.list_view);
+        toggleButton1.setOnCheckedChangeListener(this);
+
+
+        this.servicesInWelsh = new ArrayList<>();
+        servicesInWelsh.add("Minor Ailments");
+        servicesInWelsh.add("Flu Vaccinations");
+        servicesInWelsh.add("NHS Health Check");
+        servicesInWelsh.add("Stop Smoking Service");
+        servicesInWelsh.add("Advice on Alcohol Consumption");
+
+        int i;
+        for(i=0; i < servicesInWelsh.size(); i++) {
+            System.out.println(i);
+        }
+
+        this.arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                this.servicesInWelsh);
+        this.listView.setAdapter(this.arrayAdapter);
     }
 
 
@@ -30,11 +68,11 @@ public class ServicesInWelshActivity extends AppCompatActivity implements Compou
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if (isChecked) {
 
-            relativeLayout.setBackgroundColor(Color.BLUE);
+            toggleButton1.setBackgroundColor(Color.BLUE);
 
         }else {
 
-            relativeLayout.setBackgroundColor(Color.GRAY);
+            toggleButton1.setBackgroundColor(Color.GRAY);
 
         }
     }
