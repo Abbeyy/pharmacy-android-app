e2221344e4bec8194f8c39a9e80254b8d8b31022
Aschwin <sivaparana@cardiff.ac.uk>
2018-03-20 05:12:45 +0000

    Functions

--- app/src/main/java/com/nsa/welshpharmacy/MainMenu.java
+++ app/src/main/java/com/nsa/welshpharmacy/MainMenu.java
@@ -3,11 +3,21 @@ package com.nsa.welshpharmacy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.goodiebag.pinview.Pinview;
+
 public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Pinview pinview = (Pinview)findViewById(R.id.pinView);
+        pinview.setPinViewEventListener(new Pinview.PinViewEventListener() {
+            @Override
+            public void onDataEntered(Pinview PinView, boolean b) {
+//                Toast.makeText(MainMenu.this, ""+pinview.getValue(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
