5e53ad12573f149ef00e4b927c5f1b55df8f6ccb
Ghada abughararh <c1660911@cardiff.ac.uk>
2018-03-20 12:30:48 +0000

    interaction in ratting bar and general users button

--- app/src/main/java/com/nsa/welshpharmacy/MainMenu.java
+++ app/src/main/java/com/nsa/welshpharmacy/MainMenu.java
@@ -1,8 +1,15 @@
 
 package com.nsa.welshpharmacy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 
 public class MainMenu extends AppCompatActivity {
@@ -11,6 +18,28 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-    }
 
+        Button button = findViewById(R.id.GeneralUsers);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pharmacyListView = new Intent(getApplicationContext(), FilterPreferenceActivity.class);
+                startActivity(pharmacyListView);
+            }
+        });
+
+
+        final TextView ratting = (TextView) findViewById(R.id.ratting);
+        ratting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ratting.setText("Ratting have submitted successfully ");
+                int[] i = new int[]{ R.id.RattingBar};
+            }
+
+        });
+
+
+    }
 }
+
