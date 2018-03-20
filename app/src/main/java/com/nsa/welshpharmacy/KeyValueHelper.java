33bf433da6315bc76e2183c8c2f510d3af441e0e
Holly Smith <SmithHL2@cardiff.ac.uk>
2018-03-20 21:10:23 +0000

    Tidying up the code

--- app/src/main/java/com/nsa/welshpharmacy/KeyValueHelper.java
+++ app/src/main/java/com/nsa/welshpharmacy/KeyValueHelper.java
@@ -12,6 +12,4 @@ public class KeyValueHelper {
     public static final String KEY_CHECKBOX_ALCOHOL = "checkbox_alcohol";
     public static final String KEY_SWITCH_LOCATION = "switch_location";
     public static final boolean DEFAULT_WIDGET_BOOLEAN = false;
-    //public static final String KEY_WIDGET_TEXT = "text_widget";
-    //public static final String DEFAULT_WIDGET_TEXT = "";
 }
--- app/src/main/java/com/nsa/welshpharmacy/MainMenu.java
+++ app/src/main/java/com/nsa/welshpharmacy/MainMenu.java
@@ -4,11 +4,8 @@ package com.nsa.welshpharmacy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
 import android.widget.TextView;
 
 
@@ -29,12 +26,12 @@ public class MainMenu extends AppCompatActivity {
         });
 
 
-        final TextView ratting = (TextView) findViewById(R.id.ratting);
-        ratting.setOnClickListener(new View.OnClickListener() {
+        final TextView rating = (TextView) findViewById(R.id.rating);
+        rating.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ratting.setText("Rating have submitted successfully ");
-                int[] i = new int[]{ R.id.RattingBar};
+                rating.setText("Rating have submitted successfully ");
+                int[] i = new int[]{ R.id.RatingBar};
             }
 
         });
