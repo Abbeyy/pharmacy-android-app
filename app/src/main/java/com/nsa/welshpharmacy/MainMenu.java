fb1be7c38b70a23de7dce47d10998152fbd2b2c9
Ghada abughararh <c1660911@cardiff.ac.uk>
2018-03-29 13:11:23 +0100

    RatingBar

--- app/src/main/java/com/nsa/welshpharmacy/MainMenu.java
+++ app/src/main/java/com/nsa/welshpharmacy/MainMenu.java
@@ -6,15 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
-
-
+import android.widget.Toast;
 public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
 
         Button button = findViewById(R.id.GeneralUsers);
         button.setOnClickListener(new View.OnClickListener() {
@@ -24,19 +25,24 @@ public class MainMenu extends AppCompatActivity {
                 startActivity(pharmacyListView);
             }
         });
+        final TextView submitRating = (TextView)findViewById(R.id.rating);
+        final RatingBar rating = (RatingBar) findViewById(R.id.RatingBar);
+        submitRating.setClickable(true);
+        submitRating.setOnClickListener(new View.OnClickListener() {
 
-
-        final TextView rating = (TextView) findViewById(R.id.rating);
-        rating.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                rating.setText("Rating have submitted successfully ");
-                int[] i = new int[]{ R.id.RatingBar};
+            public void onClick(View v){
+                submitRating.setText("Rating has been submitted successfully ");
+                String totalStars = "Total Stars:: " + rating.getNumStars();
+                String ratingr = "Rating :: " + rating.getRating();
+                Toast.makeText(getApplicationContext(), totalStars + "\n" + ratingr, Toast.LENGTH_LONG).show();
             }
 
         });
 
 
     }
+
 }
+
 
