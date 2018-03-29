4eac74bc1089c19e9e9b6afffc00f167e2c30e17
Abbey Ross <RossA9@Cardiff.ac.uk>
2018-03-29 15:07:24 +0100

    Fixed fragments - have one fully working fragment via FragmentManager (programatical-way). Help gained from lecturer Liam. Next step is task 5 of US 71.

--- app/src/main/java/com/nsa/welshpharmacy/ListPharmaciesActivity.java
+++ app/src/main/java/com/nsa/welshpharmacy/ListPharmaciesActivity.java
@@ -1,28 +1,13 @@
 package com.nsa.welshpharmacy;
 
-import android.app.FragmentManager;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.annotation.StringRes;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.widget.ListViewCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
-import com.nsa.welshpharmacy.mockingdata.Pharmacy;
-
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.text.SimpleDateFormat;
-import java.util.Vector;
 
 /**
  * Created by c1714546 on 3/14/2018.
@@ -44,32 +29,15 @@ public class ListPharmaciesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_pharmacies_main);
-        //activity should be inflating list_pharmacies - mostly empty layout
+        setContentView(R.layout.list_pharmacies_activity_layout);
+        //activity should be inflating list_pharmacies_activity_layout - mostly empty layout
         //due to use of fragments - but that doesnt work? This does?.... fix!
 
-        //date stuff
-        setUpDate(); //line 62
-
         //setting up main fragment of view
-        Fragment fragment_main = new ListPharmaciesMainFragment();
-
         android.support.v4.app.FragmentManager fmtManager = getSupportFragmentManager();
         android.support.v4.app.FragmentTransaction fmtTransaction = fmtManager.beginTransaction();
-        fmtTransaction.add(R.id.fragments_container, fragment_main);
+        fmtTransaction.add(R.id.fragments_container, new ListPharmaciesMainFragment());
         fmtTransaction.commit();
-    }
-
-    public void setUpDate() {
-        //Code help gathered from:
-        // https://stackoverflow.com/questions/40310773/android-studio-textview-show-date
-        TextView dateTV = (TextView)findViewById(R.id.date_text_view);
-
-        Date todaysDate = Calendar.getInstance().getTime();
-        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
-        String date_today = format.format(todaysDate);
-
-        dateTV.setText("  Today's Date: " + date_today);
     }
 
 }
--- app/src/main/java/com/nsa/welshpharmacy/ListPharmaciesMainFragment.java
+++ app/src/main/java/com/nsa/welshpharmacy/ListPharmaciesMainFragment.java
@@ -9,11 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nsa.welshpharmacy.mockingdata.Pharmacy;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Vector;
 
@@ -40,8 +44,8 @@ public class ListPharmaciesMainFragment extends Fragment implements AdapterView.
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.list_pharmacies_main, container, false);
-        //inflating layout list_pharmacies_main as layout for my fragment, holding both
+        View v = inflater.inflate(R.layout.list_pharmacies_fragment_one_layout, container, false);
+        //inflating layout list_pharmacies_fragment_one_layout as layout for my fragment, holding both
         //textviews and the listview!
 
 
@@ -60,11 +64,26 @@ public class ListPharmaciesMainFragment extends Fragment implements AdapterView.
         );
 
         this.lView.setAdapter(this.arrayAdpt);
-        //this.lView.setOnItemClickListener(this);
+        this.lView.setOnItemClickListener(this);
         //line 80
+
+        //date stuff
+        setUpDate(v);
 
 
         return v;
+    }
+
+    public void setUpDate(View v) {
+        //Code help gathered from:
+        // https://stackoverflow.com/questions/40310773/android-studio-textview-show-date
+        TextView dateTV = (TextView)v.findViewById(R.id.date_text_view);
+
+        Date todaysDate = Calendar.getInstance().getTime();
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        String date_today = format.format(todaysDate);
+
+        dateTV.setText("  Today's Date: " + date_today);
     }
 
     public void generatePharmacies(int numOfPharmacies) {
