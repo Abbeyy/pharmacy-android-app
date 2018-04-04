df7137255744c592db68405f0b2bc07b96dfa091
Abbey Ross <RossA9@Cardiff.ac.uk>
2018-04-04 18:50:01 +0100

    Updated second fragment to read from shared preferences of mother activity. Tested app and it successfully updates an added textview on this fragment of the data originally put and retrieved from shared preferences.

--- app/src/main/java/com/nsa/welshpharmacy/view/listpharmacies/ListPharmacysDetailsFragment.java
+++ app/src/main/java/com/nsa/welshpharmacy/view/listpharmacies/ListPharmacysDetailsFragment.java
@@ -1,5 +1,7 @@
 package com.nsa.welshpharmacy.view.listpharmacies;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -29,6 +31,12 @@ public class ListPharmacysDetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.list_pharmacys_details_fragment_two_layout, container, false);
+
+        SharedPreferences sharedPrefs = this.getActivity().getPreferences(Context.MODE_PRIVATE);
+        int pharmacyPosition = sharedPrefs.getInt("position", -1);
+        AppCompatTextView positionTV = (AppCompatTextView)v.findViewById(R.id.position_textview);
+        positionTV.setText(Integer.toString(pharmacyPosition));
+        //Reminder, 1st listed item will have a position of 0.
 
         return v;
     }
