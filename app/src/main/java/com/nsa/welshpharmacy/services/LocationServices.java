058975e4189624601949a9ffd27901e7f7d71099
Holly Smith <SmithHL2@cardiff.ac.uk>
2018-04-05 13:12:34 +0100

    Added comments

--- app/src/main/java/com/nsa/welshpharmacy/services/LocationServices.java
+++ app/src/main/java/com/nsa/welshpharmacy/services/LocationServices.java
@@ -15,6 +15,9 @@ import java.io.IOException;
 import java.util.List;
 
 /**
+ * This service allows the location of the user to be retrieved whether they enter a valid postcode
+ * or opt to allow their phone network to be used
+ *
  * Created by c1712480 on 03/04/2018.
  */
 
@@ -25,7 +28,7 @@ public class LocationServices {
         return userLocation;
     }
 
-    public static void loadPhoneLocationViaNetwork(Context context) {
+    public static LatLng loadPhoneLocationViaNetwork(Context context) {
         // Documentation: https://developer.android.com/guide/topics/location/strategies.html
         //Retrieve the users last known location via their network
         // Acquire a reference to the system Location Manager
@@ -41,15 +44,16 @@ public class LocationServices {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            return;
+            return null;
         }
         //userLocation = locationManager.getLastKnownLocation(locationProvider);
         userLocation = new LatLng(locationManager.getLastKnownLocation(locationProvider).getLatitude(),
                 locationManager.getLastKnownLocation(locationProvider).getLongitude());
+        return userLocation;
     }
 
     //TODO loadPhoneLocationViaPostcode
-    public static void loadPhoneLocationViaPostcode(Context context, String userPostcode){
+    public static LatLng loadPhoneLocationViaPostcode(Context context, String userPostcode){
 
         //Adapted from: https://stackoverflow.com/a/4833943 Retrieved: 3/4/18
         final Geocoder geocoder = new Geocoder(context);
@@ -62,9 +66,11 @@ public class LocationServices {
                 //        address.getLatitude(), address.getLongitude());
                 //Toast.makeText(context, message, Toast.LENGTH_LONG).show();
                 userLocation = new LatLng(address.getLatitude(), address.getLongitude());
+                return userLocation;
             }
         } catch (IOException e) {
             // handle exception
         }
+        return userLocation;
     }
 }
