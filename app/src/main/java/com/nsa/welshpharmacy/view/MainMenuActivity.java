8d58c2635736619b4e4053be81e7f8d549abfe72
Abbey Ross <RossA9@Cardiff.ac.uk>
2018-04-21 15:09:10 +0100

    Large commit. Edited button id in mainmenuactivity to grab and add onclicklistener. Inside listener, call new class created involving a switch to decide what language resources to draw upon. Remainder of onclick restarts activity and remaining app also translated! However, need to add sharedprefs to remember switch has been clicked, and add functionality to translate back to english.

--- app/src/main/java/com/nsa/welshpharmacy/view/MainMenuActivity.java
+++ app/src/main/java/com/nsa/welshpharmacy/view/MainMenuActivity.java
@@ -1,17 +1,25 @@
 
 package com.nsa.welshpharmacy.view;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nsa.welshpharmacy.R;
+import com.nsa.welshpharmacy.model.Language;
+import com.nsa.welshpharmacy.view.language.LanguageManager;
 
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    private String welshLanguageCode = "cy";
+    private String englishLanguageCode = "en";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +46,20 @@ public class MainMenuActivity extends AppCompatActivity {
 
         });
 
+        Switch changeLang = (Switch)findViewById(R.id.change_language);
+        changeLang.setOnCheckedChangeListener(this);
 
     }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Toast.makeText(this, "Changing language...", Toast.LENGTH_SHORT).show();
+        LanguageManager.changeLang(this.getResources(), welshLanguageCode);
+
+        Intent restartActivity = getIntent();
+        finish();
+        startActivity(restartActivity);
+    }
+
 }
 
