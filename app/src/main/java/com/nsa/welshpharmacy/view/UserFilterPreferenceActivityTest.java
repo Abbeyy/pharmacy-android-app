907f6c30057dd74c061f523a76445ea3b64013aa
Mukhtar Mohamed <Mohamedm4@cardiff.ac.uk>
2018-04-21 12:35:26 +0100

    I have added the code into UserFilterPreferenceActivityTest but bugs are present

--- app/src/main/java/com/nsa/welshpharmacy/view/UserFilterPreferenceActivityTest.java
+++ app/src/main/java/com/nsa/welshpharmacy/view/UserFilterPreferenceActivityTest.java
@@ -1,9 +1,17 @@
 package com.nsa.welshpharmacy.view;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
@@ -18,10 +26,45 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nsa.welshpharmacy.R;
+import com.nsa.welshpharmacy.services.LocationServices;
+import com.nsa.welshpharmacy.view.listPharmacies.ListPharmaciesActivity;
 
-public class UserFilterPreferenceActivityTest extends AppCompatActivity {
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class UserFilterPreferenceActivityTest extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener, SharedPreferences.OnSharedPreferenceChangeListener {
+
+    //code for user filter preference activity
+    private AppCompatCheckBox checkMinorAilments;
+    private AppCompatCheckBox checkFluVac;
+    private AppCompatCheckBox checkHealthCheck;
+    private AppCompatCheckBox checkSmoking;
+    private AppCompatCheckBox checkAlcohol;
+    private AppCompatEditText textPostcodeWidget;
+    private SwitchCompat switchOnLocationWidget;
+
+    private AppCompatEditText postcodeET;
+
+    private AppCompatButton submitButton;
+    private AppCompatButton resetButton;
+
+    private SharedPreferences sharedPreferences;
+
+    private static final int REQUEST_FINE_LOCATION = 1;
+
+
+
+
+
+
+
+
+
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -113,10 +156,34 @@ public class UserFilterPreferenceActivityTest extends AppCompatActivity {
 
             }
         });
+
+
+
+        //code for user filter preference
+        this.checkMinorAilments = this.findViewById(R.id.check_minor_ailments);
+        this.checkFluVac = this.findViewById(R.id.check_flu_vaccines);
+        this.checkHealthCheck = this.findViewById(R.id.check_health_check);
+        this.checkSmoking = this.findViewById(R.id.check_smoking);
+        this.checkAlcohol = this.findViewById(R.id.check_alcohol);
+        this.textPostcodeWidget = this.findViewById(R.id.text_postcode);
+        this.switchOnLocationWidget = this.findViewById(R.id.switch_location);
+
+        this.submitButton = this.findViewById(R.id.submit_button);
+        this.resetButton = this.findViewById(R.id.reset_button);
+
+        this.sharedPreferences = this.getPreferences(MODE_PRIVATE);
+
+        initValues();
+
+        textPostcodeWidget.setOnClickListener(this);
+        submitButton.setOnClickListener(this);
+        resetButton.setOnLongClickListener(this);
+
+        postcodeET = (AppCompatEditText)findViewById(R.id.text_postcode);
+        postcodeET.setOnClickListener(this);
+
+
     }
-
-
-
 
 
     @Override
@@ -199,4 +266,135 @@ public class UserFilterPreferenceActivityTest extends AppCompatActivity {
             return 3;
         }
     }
+
+
+
+
+    //code for user filter preference
+    private void initValues(){
+        if (this.sharedPreferences != null) {
+            this.checkMinorAilments.setChecked(sharedPreferences.getBoolean(KeyValueHelper.KEY_CHECKBOX_AILMENTS, KeyValueHelper.DEFAULT_WIDGET_BOOLEAN));
+            this.checkFluVac.setChecked(sharedPreferences.getBoolean(KeyValueHelper.KEY_CHECKBOX_FLU, KeyValueHelper.DEFAULT_WIDGET_BOOLEAN));
+            this.checkHealthCheck.setChecked(sharedPreferences.getBoolean(KeyValueHelper.KEY_CHECKBOX_HEALTH, KeyValueHelper.DEFAULT_WIDGET_BOOLEAN));
+            this.checkSmoking.setChecked(sharedPreferences.getBoolean(KeyValueHelper.KEY_CHECKBOX_SMOKING, KeyValueHelper.DEFAULT_WIDGET_BOOLEAN));
+            this.checkAlcohol.setChecked(sharedPreferences.getBoolean(KeyValueHelper.KEY_CHECKBOX_ALCOHOL, KeyValueHelper.DEFAULT_WIDGET_BOOLEAN));
+            this.switchOnLocationWidget.setChecked(sharedPreferences.getBoolean(KeyValueHelper.KEY_SWITCH_LOCATION, KeyValueHelper.DEFAULT_WIDGET_BOOLEAN));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (this.sharedPreferences != null)
+            this.sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (this.sharedPreferences != null)
+            this.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        int id = view.getId();
+        //Adapted from https://stackoverflow.com/a/4531500 Retrieved: 17/3/18
+        //Adapted from https://stackoverflow.com/a/8204716 Retrieved 17/3/18
+        Pattern POSTCODE_REGEX = Pattern.compile(getString(R.string.postcode_regex));
+
+        if (id == R.id.text_postcode) {
+            postcodeET.setText("");
+        }
+
+        Matcher matcher = POSTCODE_REGEX.matcher(textPostcodeWidget.getText());
+        //If there is not a valid postcode and the switch is not checked show a toast warning
+        //And if both a valid postcode and the switch is checked show the toast warning
+        if((!matcher.matches() && !switchOnLocationWidget.isChecked()) || (matcher.matches() && switchOnLocationWidget.isChecked())){
+            Toast.makeText(this, R.string.enter_valid_location, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(id == R.id.submit_button && matcher.matches()){
+            try {
+                LocationServices.loadPhoneLocationViaPostcode(this, textPostcodeWidget.toString());
+            }catch (IOException e){
+                Toast.makeText(this, R.string.location_catch_statement, Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Intent pharmacyListView = new Intent(this, ListPharmaciesActivity.class);
+            startActivity(pharmacyListView);
+        }
+        if(id == R.id.submit_button && switchOnLocationWidget.isChecked()){
+            if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                if (LocationServices.isNetworkEnabled(this)){
+                    //If fine location permission is already granted then update last known location from network
+                    LocationServices.loadPhoneLocationViaNetwork(this);
+                    Toast.makeText(this, LocationServices.getUserLocation().toString(), Toast.LENGTH_LONG).show();
+                    //Then switch view to next activity
+                    Intent pharmacyListView = new Intent(this, ListPharmaciesActivity.class);
+                    startActivity(pharmacyListView);
+                }
+            } else{
+                //Fine location permission has not been granted
+                //Rationale as to why the user should grant permission
+                if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){
+                    Toast.makeText(this, "Fine location permission is needed to retrieve the location.",
+                            Toast.LENGTH_SHORT).show();
+                }
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+            }
+        }
+        //If the submit button is pressed and the shared preferences are null add the shared preferences
+        if (id == R.id.submit_button && this.sharedPreferences != null){
+            SharedPreferences.Editor editor = this.sharedPreferences.edit();
+            editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_AILMENTS, this.checkMinorAilments.isChecked());
+            editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_FLU, this.checkFluVac.isChecked());
+            editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_HEALTH, this.checkHealthCheck.isChecked());
+            editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_SMOKING, this.checkSmoking.isChecked());
+            editor.putBoolean(KeyValueHelper.KEY_CHECKBOX_ALCOHOL, this.checkAlcohol.isChecked());
+            editor.putBoolean(KeyValueHelper.KEY_SWITCH_LOCATION, this.switchOnLocationWidget.isChecked());
+            editor.apply();
+        }
+    }
+    @Override
+    public boolean onLongClick(View view){
+        int id = view.getId();
+
+        if (id == R.id.reset_button && this.sharedPreferences != null) {
+            //Reset values to their default
+            this.sharedPreferences.edit().clear().apply();
+            //Pop up toast message to say that values have been reset
+            Toast.makeText(this, getString(R.string.reset_text), Toast.LENGTH_SHORT).show();
+            //Initialise sharedPreferences
+            initValues();
+        }
+        return true;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s){
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == REQUEST_FINE_LOCATION) {
+            //Check if the permission has been granted
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                LocationServices.loadPhoneLocationViaNetwork(this);
+            }else{
+                //Fine permission was denied so the feature cannot be used
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+
+
+
+
+
+
 }
