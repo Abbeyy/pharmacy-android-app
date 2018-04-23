543211c52fb33771a53638dee5fd31e5c7601791
Holly Smith <SmithHL2@cardiff.ac.uk>
2018-04-23 11:39:41 +0100

    Refactored Pharmacy to be Parceable and added maven to gradle

--- app/src/main/java/com/nsa/welshpharmacy/model/Pharmacy.java
+++ app/src/main/java/com/nsa/welshpharmacy/model/Pharmacy.java
@@ -1,23 +1,39 @@
 package com.nsa.welshpharmacy.model;
 
 import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.IgnoreExtraProperties;
 import com.nsa.welshpharmacy.services.LocationServices;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Represents all of the relevant information about a pharmacy
  * The fields correspond with the information contained in the db
  *
+ * Parcelable example: http://www.vogella.com/tutorials/AndroidParcelable/article.html
+ *
  * Created by c1714546 on 3/23/2018.
  */
 
 @IgnoreExtraProperties
-public class Pharmacy {
+public class Pharmacy  implements Parcelable {
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator(){
+        public Pharmacy createFromParcel(Parcel in){
+            return new Pharmacy(in);
+        }
+
+        public Pharmacy[] newArray(int size){
+            return new Pharmacy[size];
+        }
+    };
+
     public String id;
     public String name;
     public String phone;
@@ -66,6 +82,33 @@ public class Pharmacy {
 
     public String getWebsite() {
         return website;
+    }
+
+    //Parcel constructor
+    public Pharmacy(Parcel in) {
+        id = in.readString();
+        name = in.readString();
+        phone = in.readString();
+        postcode = in.readString();
+        //https://stackoverflow.com/questions/43471996/pass-parcelable-object-containing-map-between-activity/43478206#43478206
+        services = new HashMap<String, PharmacyServiceAvailability>();
+        in.readMap(services, PharmacyServiceAvailability.class.getClassLoader());
+        website = in.readString();
+    }
+
+    @Override
+    public int describeContents(){
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags){
+        dest.writeString(id);
+        dest.writeString(name);
+        dest.writeString(phone);
+        dest.writeString(postcode);
+        dest.writeMap(services);
+        dest.writeString(website);
     }
 
     /**
