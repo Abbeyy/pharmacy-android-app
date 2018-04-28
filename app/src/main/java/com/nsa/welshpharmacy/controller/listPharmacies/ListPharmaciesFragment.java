06644fdc91fd9eb3e603a033eea2a2cc5493677e
Holly Smith <SmithHL2@cardiff.ac.uk>
2018-04-28 20:11:03 +0100

    Implemented viewModel fro ListPharmacies

--- app/src/main/java/com/nsa/welshpharmacy/controller/listPharmacies/ListPharmaciesFragment.java
+++ app/src/main/java/com/nsa/welshpharmacy/controller/listPharmacies/ListPharmaciesFragment.java
@@ -1,5 +1,7 @@
 package com.nsa.welshpharmacy.controller.listPharmacies;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -14,14 +16,10 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
 import com.nsa.welshpharmacy.R;
 import com.nsa.welshpharmacy.model.Pharmacy;
-import com.nsa.welshpharmacy.services.FirebaseServices;
+import com.nsa.welshpharmacy.viewModel.ListPharmaciesViewModel;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -33,11 +31,12 @@ import java.util.List;
  * Created by c1714546 on 3/18/2018.
  */
 
-public class ListPharmaciesFragment extends Fragment implements AdapterView.OnItemClickListener, ValueEventListener {
+public class ListPharmaciesFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     ListViewCompat lv;
-    List<Pharmacy> pharmacies;
+    List<Pharmacy> listOfPharmacies;
     List<String> listOfNames;
+    private ListPharmaciesViewModel mViewModel;
     private ArrayAdapter<String> la;
 
     private FragmentManager fmtManager;
@@ -58,13 +57,31 @@ public class ListPharmaciesFragment extends Fragment implements AdapterView.OnIt
 
         currentLang = getActivity().getSharedPreferences("currentLanguage", Context.MODE_PRIVATE);
         currentLocale = currentLang.getString("state", "default");
-
-        lv = v.findViewById(R.id.listview_pharmacies); //line 64
-
-        pharmacies = new ArrayList<>();
+        mViewModel = ViewModelProviders.of(this).get(ListPharmaciesViewModel.class);
+        listOfPharmacies = new ArrayList<>();
         listOfNames = new ArrayList<>();
 
-        FirebaseServices.loadPharmacies(this);
+        /**
+         * Retrieves the pharmacy information from the PharmacyListViewModel
+         * Adds the pharmacy data into a list of pharmacies and a list of pharmacy names
+         * The list adapter is then notified of the data change
+         */
+        final Observer<List<Pharmacy>> pharmacyObserver = new Observer<List<Pharmacy>>(){
+            @Override
+            public void onChanged(@Nullable final List<Pharmacy> pharmacies) {
+                if(pharmacies != null){
+                    listOfPharmacies.addAll(pharmacies);
+                    listOfNames.clear();
+                    for(Pharmacy pharmacy : pharmacies){
+                        listOfNames.add(pharmacy.getName());
+                    }
+                    la.notifyDataSetChanged();
+                }
+            }
+        };
+        mViewModel.getPharmacies().observe(this, pharmacyObserver);
+
+        lv = v.findViewById(R.id.listview_pharmacies);
 
         la = new ArrayAdapter<String>(
                 getActivity(),
@@ -97,29 +114,12 @@ public class ListPharmaciesFragment extends Fragment implements AdapterView.OnIt
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        //1. Toast
-        if (currentLocale == "cy") {
-            Toast.makeText(getActivity(),
-                    String.format("Defnyddiwr wedi dewis %s", lv.getItemAtPosition(position)),
-                    Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(getActivity(),
-                    String.format("User has selected %s", lv.getItemAtPosition(position)),
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        //2. Switch Fragments
+        //Switch Fragments
         expandPharmacyInfo(position);
     }
 
     public void expandPharmacyInfo(int position) {
-        //First updateSharedPrefs to store position data in activity.
-        /*
-        SharedPreferences sharedPrefs = this.getActivity().getSharedPreferences("pharmacyPos", Context.MODE_PRIVATE);
-        SharedPreferences.Editor edit = sharedPrefs.edit();
-        edit.putInt("position", position);
-        edit.apply();
-        */
+        List<Pharmacy> pharmacies = listOfPharmacies;
         // https://stackoverflow.com/a/46298244
         Bundle bundle = new Bundle();
         bundle.putParcelable("selectedPharmacy", pharmacies.get(position));
@@ -132,24 +132,5 @@ public class ListPharmaciesFragment extends Fragment implements AdapterView.OnIt
         fmtTrans.replace(R.id.fragments_container, listPharmacyDetailsFragment).addToBackStack("fragTwo");
         fmtTrans.addToBackStack(null);
         fmtTrans.commit();
-    }
-
-    @Override
-    public void onDataChange(DataSnapshot dataSnapshot){
-        // see:  https://firebase.google.com/docs/database/android/lists-of-data#listen_for_value_events
-        // Retrieve all  Pharmacy records from the Firebase database in one go
-        for(DataSnapshot pharmacySnapshot : dataSnapshot.getChildren()){
-            Pharmacy pharmacy = pharmacySnapshot.getValue(Pharmacy.class);
-            pharmacy.setId(pharmacySnapshot.getKey());
-            System.out.println("pharmacy : " + pharmacy);
-            pharmacies.add(pharmacy);
-            listOfNames.add(pharmacy.getName());
-        }
-        la.notifyDataSetChanged();
-    }
-
-    @Override
-    public void onCancelled(DatabaseError databaseError){
-        System.out.println("The read failed: " + databaseError.getCode());
     }
 }
