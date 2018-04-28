70225e084a93147cfda67cc8173321b2936a1a29
Holly Smith <SmithHL2@cardiff.ac.uk>
2018-04-28 21:22:49 +0100

    Refactored code so stores no longer needed

--- app/src/main/java/com/nsa/welshpharmacy/services/FirebaseServices.java
+++ app/src/main/java/com/nsa/welshpharmacy/services/FirebaseServices.java
@@ -5,7 +5,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.nsa.welshpharmacy.model.Language;
 import com.nsa.welshpharmacy.model.PharmacyService;
 
 import java.util.ArrayList;
@@ -18,35 +17,6 @@ import java.util.List;
  */
 
 public class FirebaseServices {
-
-    public static List<Language> loadLanguages(){
-        final ArrayList<Language> languages = new ArrayList<Language>();
-
-        //Get a reference to the services
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("languages");
-
-        // Attach a listener to read the data at the languages reference
-        ref.addValueEventListener(new ValueEventListener(){
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot){
-                // see:  https://firebase.google.com/docs/database/android/lists-of-data#listen_for_value_events
-                // Retrieve all  languages from the Firebase database in one go
-                for(DataSnapshot languageSnapshot : dataSnapshot.getChildren()){
-                    Language language = languageSnapshot.getValue(Language.class);
-                    language.setId(languageSnapshot.getKey());
-                    //System.out.println(language);
-                    languages.add(language);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError){
-                System.out.println("The read failed: " + databaseError.getCode());
-            }
-        });
-        return languages;
-    }
 
     public static void loadPharmacies(ValueEventListener listener){
         //Get a reference to the pharmacies
