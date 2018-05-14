104d1870f3972b60bdbe5fa6f7cbfec83020d8f8
Holly Smith <SmithHL2@cardiff.ac.uk>
2018-05-14 19:41:43 +0100

    Utils junit test

--- app/src/main/java/com/nsa/welshpharmacy/model/Utils.java
+++ app/src/main/java/com/nsa/welshpharmacy/model/Utils.java
@@ -3,7 +3,6 @@ package com.nsa.welshpharmacy.model;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -15,14 +14,14 @@ import java.util.stream.Collectors;
 public class Utils {
 
     /**
-     * Sorts a map by descending values
+     * Sorts a map by values with the smallest value being at  the top of the list
+     * Retrieved from: https://stackoverflow.com/a/2581754
      */
-//Retrieved from: https://stackoverflow.com/a/2581754
     @RequiresApi(api = Build.VERSION_CODES.N)
     public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
         return map.entrySet()
                 .stream()
-                .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
+                .sorted(Map.Entry.comparingByValue())
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         Map.Entry::getValue,
